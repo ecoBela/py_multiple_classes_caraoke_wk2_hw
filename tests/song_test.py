5d826c4add917dd0c547e5b856e0aa6eb385ca38
Bela <belahamid@gmail.com>
2020-11-29 16:27:47 +0000

    song has title - test passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -1,6 +1,10 @@
- import unittest
+import unittest
 
- from src/song import Song
+from src.song import Song
 
- class TestSong(unittest.TestCase):
-     
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        self.song = Song("Senorita", "Cabello_and_Mendes")
+    
+    def test_song_has_title(self):
+        self.assertEqual("Senorita", self.song.title)
