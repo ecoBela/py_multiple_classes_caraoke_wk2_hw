c23e4046f46c8947b6adda039fd0d4e81f4865b5
Bela <belahamid@gmail.com>
2020-11-29 16:45:50 +0000

    test passed: guest has cash amount

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,7 +5,10 @@ from src.song import Song
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest("Mr Bond", "£2000")
+        self.guest = Guest("Mr Bond", 2000)
 
     def test_guest_has_name(self):
         self.assertEqual("Mr Bond", self.guest.name)
+
+    def test_guest_has_cash_amount(self):
+        self.assertEqual(2000, self.guest.cash_amount)
